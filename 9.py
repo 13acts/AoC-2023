90589a298c01068a7c9a1b2b2f3cc48bb5859340
13acts <tvk872000@gmail.com>
2023-12-10 04:30:03 +0700

    Refactored

--- 9.py
+++ 9.py
@@ -1,35 +1,32 @@
 with open('data/9.txt') as f:
-	alls = list(map(str.split, f.readlines()))
-	for i in range(len(alls)):
-		alls[i] = list(map(int, alls[i]))
+	seqs = list(map(str.split, f.readlines()))
+	for i in range(len(seqs)):
+		seqs[i] = list(map(int, seqs[i]))
+
+def next_line(current):
+	return [current[i+1] - current[i] for i in range(len(current)-1)]
 
 def p1():
-	total = sum(x[-1] for x in alls)
-	for seq in alls:
+	total = sum(x[-1] for x in seqs)
+	for seq in seqs:
 		current = seq
 		while any(x!=0 for x in current):
-			tmp_current = []
-			for i in range(len(current)-1):
-				tmp_current += [current[i+1] - current[i]]
-			current = tmp_current.copy()
+			current = next_line(current)
 			total += current[-1]
 	print(total)
 
 def p2():
 	total = 0
-	for seq in alls:
+	for seq in seqs:
 		firsts = [seq[0]]
 		current = seq
 		while any(x!=0 for x in current):
-			tmp_current = []
-			for i in range(len(current)-1):
-				tmp_current += [current[i+1] - current[i]]
-			current = tmp_current.copy()
+			current = next_line(current)
 			firsts += [current[0]]
-		tmp = 0
+		first = 0
 		while firsts:
-			tmp = firsts.pop() - tmp
-		total += tmp
+			first = firsts.pop() - first
+		total += first
 	print(total)
 
 p1()
