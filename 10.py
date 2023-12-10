da689c74598572554c13d5b41b3c97c9944f4f0b
13acts <tvk872000@gmail.com>
2023-12-10 16:46:08 +0700

    Refactored P1

--- 10.py
+++ 10.py
@@ -1,10 +1,9 @@
 I = '|'
 H = '-'
 F = 'F'
+T = '7'
 J = 'J'
 L = 'L'
-T = '7'
-
 S = 'S'
 
 class Layout:
@@ -16,52 +15,38 @@ class Layout:
     def __str__(self) -> str:
         return self.data
     
-    def start(self):
+    def start(self) -> tuple:
         for a in range(self.w):
             for b in range(self.h):
                 if data[a][b] == S:
                     return (a,b)        
 
 
-
 class Node:
-    def __init__(self, coord):
+    def __init__(self, coord) -> None:
         self.coord = coord
         self.label = layout.data[coord[0]][coord[1]]
 
     def __str__(self) -> str:
         return f'{self.coord}: {self.label}'
 
-    def next_node(self, previous:'Node'=None):
+    def next_node(self, previous:'Node'=None) -> tuple:
         i, j = self.coord
         match self.label:
             case '|':
-                # if i in (0, layout.h):
-                #     return
                 return [x for x in [(i+1, j), (i-1, j)] if x != previous.coord][0]
             case '-':
-                # if j in (0, layout.w):
-                #     return
                 return [x for x in [(i, j+1), (i, j-1)] if x != previous.coord][0]
             case 'F':
-                # if i == layout.h or j == layout.w:
-                #     return
                 return [x for x in [(i+1, j), (i, j+1)] if x != previous.coord][0]
             case '7':
-                # if i == layout.h or j == 0:
-                #     return
                 return [x for x in [(i+1, j), (i, j-1)] if x != previous.coord][0]
             case 'J':
-                # if i == 0 or j == 0:
-                #     return
                 return [x for x in [(i-1, j), (i, j-1)] if x != previous.coord][0]
             case 'L':
-                # if i == 0 or j == layout.w:
-                #     return
                 return [x for x in [(i-1, j), (i, j+1)] if x != previous.coord][0]
             case 'S':
-                raw_options = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
-                options = raw_options[:]
+                options = [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]
                 if i == 0:
                     options.remove((i-1, j))
                 elif i == layout.h:
@@ -69,12 +54,14 @@ class Node:
                 if j == 0:
                     options.remove((i, j-1))
                 elif j == layout.w:
-                    options.remove((i, j+1))
-                
-                valid = [[I, J, L], [I, F, T], [H, T, J], [H, L, F]]
-                for option in options:
-                    if layout.data[option[0]][option[1]] in valid[raw_options.index(option)]:
-                        return option
+                    options.remove((i, j+1))                
+                valid = {(i+1, j): [I, J, L],
+                         (i-1, j): [I, F, T],
+                         (i, j+1): [H, T, J],
+                         (i, j-1): [H, L, F]}
+                for coord in options:
+                    if layout.data[coord[0]][coord[1]] in valid[coord]:
+                        return coord
 
 
 with open('data/10.txt') as f:
@@ -91,5 +78,4 @@ while next_node.label != S:
     next_node = Node(next_node.next_node(previous=previous))
     count += 1
 
-print(count)
 print((count//2)+1)
